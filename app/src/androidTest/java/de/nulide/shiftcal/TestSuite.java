8c1ebdecd58cb7eca29398a8da0a9446f86be618
guruizhen <ravenna1031@gmail.com>
2022-11-11 15:25:33 +0000

    add jacoco plugin

---
+++ app/src/androidTest/java/de/nulide/shiftcal/TestSuite.java
@@ -0,0 +1,23 @@
+package de.nulide.shiftcal;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestSuite {
+
+    @Rule
+    public ActivityScenarioRule<CalendarActivity> activityScenarioRule = new ActivityScenarioRule<>(CalendarActivity.class);
+
+    @Test
+    public void testTest() {
+        onView(withId(R.id.btnPopup)).perform(click());
+    }
+}
