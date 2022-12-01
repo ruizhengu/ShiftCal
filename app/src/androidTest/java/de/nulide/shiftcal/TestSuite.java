0cc60d9b6678b5e5f3faa5c1790d0af00d3f7f42
guruizhen <ravenna1031@gmail.com>
2022-12-01 16:50:32 +0000

    instruction - add assertion

--- app/src/androidTest/java/de/nulide/shiftcal/TestSuite.java
+++ app/src/androidTest/java/de/nulide/shiftcal/TestSuite.java
@@ -32,9 +32,10 @@ public class TestSuite {
         // 3. Click the "add" icon in the bottom right corner.
         // 4. Enter "Sheffield" in the "Name" field.
         // 5. Click the "✓" icon in the bottom right corner.
-        // 6. Long click "Sheffield" on the "Employers" page.
-        // 7. Click "Delete" in the popup window.
-        // 8. Use "press back" to return to the home page.
+        // 6. Assert the created employer's name to "Sheffield".
+        // 7. Long click "Sheffield" on the "Employers" page.
+        // 8. Click "Delete" in the popup window.
+        // 9. Use "press back" to return to the home page.
     }
 
     @Test
@@ -47,20 +48,21 @@ public class TestSuite {
         // 6. Use "press back" to return to the home page.
         // 7. Click the "menu" icon in the top right corner of the home page.
         // 8. Click "Shifts".
-        // 8. Click the "add" icon in the bottom right corner.
-        // 9. Enter "Weekday" in the "Name" field.
-        // 10. Enter "WD" in the "Short Name" field.
-        // 11. Click the "Start Time" button, click "9" on the dial then click "OK".
-        // 12. Click the "End Time" button, click "17" on the dial then click "OK".
-        // 13. Click the "✓" icon in the bottom right corner.
-        // 14. Long click "Weekday" on the "Shifts" page.
-        // 15. Click the "Delete" in the popup window.
-        // 16. Use "press back" to return to the home page.
-        // 17. Click the "menu" icon in the top right corner of the home page.
-        // 18. Click "Employers".
-        // 19. Long click "Sheffield" on the "Employers" page.
-        // 20. Click "Delete" in the popup window.
-        // 21. Use "press back" to return to the home page.
+        // 9. Click the "add" icon in the bottom right corner.
+        // 10. Enter "Weekday" in the "Name" field.
+        // 11. Enter "WD" in the "Short Name" field.
+        // 12. Click the "Start Time" button, click "9" on the dial then click "OK".
+        // 13. Click the "End Time" button, click "17" on the dial then click "OK".
+        // 14. Click the "✓" icon in the bottom right corner.
+        // 15. Assert the created shift's name to "Weekday".
+        // 16. Long click "Weekday" on the "Shifts" page.
+        // 17. Click the "Delete" in the popup window.
+        // 18. Use "press back" to return to the home page.
+        // 19. Click the "menu" icon in the top right corner of the home page.
+        // 20. Click "Employers".
+        // 21. Long click "Sheffield" on the "Employers" page.
+        // 22. Click "Delete" in the popup window.
+        // 23. Use "press back" to return to the home page.
     }
 
     @Test
@@ -73,35 +75,35 @@ public class TestSuite {
         // 6. Use "press back" to return to the home page.
         // 7. Click the "menu" icon in the top right corner of the home page.
         // 8. Click "Shifts".
-        // 8. Click the "add" icon in the bottom right corner.
-        // 9. Enter "Weekday" in the "Name" field.
-        // 10. Enter "WD" in the "Short Name" field.
-        // 11. Click the "Start Time" button, click "9" on the dial then click "OK".
-        // 12. Click the "End Time" button, click "17" on the dial then click "OK".
-        // 13. Click the "✓" icon in the bottom right corner.
-        // 14. Use "press back" to return to the home page.
-        // 15. Click the "edit" icon on the bottom right corner.
-        // 16. Click the "S" icon above the "edit" icon.
-        // 17. Click "Weekday".
-        // 18. Click date "10" in the calendar.
-        // 19. Click the "✓" icon in the bottom right corner.
-        // 20. Launch the "Calendar" application.
-        // 21. Assert that the 10th of this month has the "Weekday" event.
-        // 22. Launch the "ShiftCal" application.
-        // 23. Click the "edit" icon on the bottom right corner.
-        // 24. Click the "S" icon above the "edit" icon.
-        // 25. Click "Delete".
-        // 26. Click date "10" in the calendar.
-        // 27. Click the "✓" icon in the bottom right corner.
-        // 28. Click the "menu" icon in the top right corner on the home page.
-        // 29. Click "Shifts".
-        // 30. Long click "Weekday" on the "Shifts" page.
-        // 31. Click the "Delete" in the popup window.
-        // 32. Use "press back" to return to the home page.
-        // 33. Click the "menu" icon in the top right corner of the home page.
-        // 34. Click "Employers".
-        // 35. Long click "Sheffield" on the "Employers" page.
-        // 36. Click "Delete" in the popup window.
-        // 37. Use "press back" to return to the home page.
+        // 9. Click the "add" icon in the bottom right corner.
+        // 10. Enter "Weekday" in the "Name" field.
+        // 11. Enter "WD" in the "Short Name" field.
+        // 12. Click the "Start Time" button, click "9" on the dial then click "OK".
+        // 13. Click the "End Time" button, click "17" on the dial then click "OK".
+        // 14. Click the "✓" icon in the bottom right corner.
+        // 15. Use "press back" to return to the home page.
+        // 16. Click the "edit" icon on the bottom right corner.
+        // 17. Click the "S" icon above the "edit" icon.
+        // 18. Click "Weekday".
+        // 19. Click date "10" in the calendar.
+        // 20. Click the "✓" icon in the bottom right corner.
+        // 21. Launch the "Calendar" application.
+        // 22. Assert that the 10th of this month has the "WD - Weekday" event.
+        // 23. Launch the "ShiftCal" application.
+        // 24. Click the "edit" icon on the bottom right corner.
+        // 25. Click the "S" icon above the "edit" icon.
+        // 26. Click "Delete".
+        // 27. Click date "10" in the calendar.
+        // 28. Click the "✓" icon in the bottom right corner.
+        // 29. Click the "menu" icon in the top right corner on the home page.
+        // 30. Click "Shifts".
+        // 31. Long click "Weekday" on the "Shifts" page.
+        // 32. Click the "Delete" in the popup window.
+        // 33. Use "press back" to return to the home page.
+        // 34. Click the "menu" icon in the top right corner of the home page.
+        // 35. Click "Employers".
+        // 36. Long click "Sheffield" on the "Employers" page.
+        // 37. Click "Delete" in the popup window.
+        // 38. Use "press back" to return to the home page.
     }
 }
