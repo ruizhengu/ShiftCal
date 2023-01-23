b576c3d01c965ea30b361157a23ab024fa0aee68
guruizhen <ravenna1031@gmail.com>
2023-01-23 13:45:48 +0000

    update to practice instruction

--- app/src/androidTest/java/de/nulide/shiftcal/TestSuite.java
+++ app/src/androidTest/java/de/nulide/shiftcal/TestSuite.java
@@ -18,48 +18,54 @@ public class TestSuite {
 
     /**
      * The application is a calendar for managing your employers and shifts.
-     * You can also sync your created shifts in Google Calendar.
-     *
-     * Before the development:
-     * Please make sure the emulator is connected to the Internet.
-     * Please login your Google account on the emulator to make sure you can access the Calendar app.
-     * Turn on the "Sync" functionality in Settings:
-     * 1. Click the "menu" icon in the top right corner of the home page.
-     * 2. Click "Settings".
-     * 3. Click "Sync".
-     * 4. Turn on the switch to enable sync functionality.
      */
 
     @Test
     public void createEmployer() {
-        /*
-        Description: Create a new Employer with the Name "Sheffield". Delete the created Employer.
-
-        Assertion:  None.
-        */
+        // 1. Click the menu icon in the top-right corner.
+        // 2. Click "Employers".
+        // 3. Click the + icon on the bottom-right corner.
+        // 4. Enter "Sheffield" in the Name field (then press enter on the keyboard).
+        // 5. Click the ✓ icon.
+        // 6. Long click "Sheffield".
+        // 7. Click "Delete".
+        // 8. Press back to return to the homepage.
     }
 
     @Test
     public void createShifts() {
-        /*
-        Description: Create a new Employer with the Name "Sheffield".
-                     Create a new Shift with the Name "Weekday", Short Name "WD", Start Time "09:00", End Time "17:00".
-                     Delete the created Shift. Delete the created Employer.
-
-        Assertion: None.
-        */
+        // 1. Create Employer "Sheffield" (steps 1 - 5 in test case createEmployer).
+        // 2. Press back to return to the homepage.
+        // 3. Click the menu icon in the top-right corner.
+        // 4. Click "Shifts".
+        // 5. Click the + icon on the bottom-right corner.
+        // 6. Enter "Weekday" in the Name field (then press enter on the keyboard).
+        // 7. Enter "WD" in the Short Name field (then press enter on the keyboard).
+        // 8. Click the button of Start Time, click 9 then click OK.
+        // 9. Click the button of End Time, click 17 then click OK.
+        // 10. Click the ✓ icon.
+        // 11. Long click "Weekday".
+        // 12. Click "Delete".
+        // 13. Press back to return to the homepage.
+        // 14. Go to the Employers page (steps 1 - 2 in test case createEmployer).
+        // 15. Delete Employer "Sheffield" then return to the homepage (steps 6 - 8 in test case createEmployer)
     }
 
     @Test
     public void setShifts() {
-        /*
-        Description: Create a new Employer with the Name "Sheffield".
-                     Create a new Shift with the Name "Weekday", Short Name "WD", Start Time "09:00", End Time "17:00".
-                     Set the Shift on 20th December 2022. Launch the Calendar application and open the created event.
-                     Go back to ShiftCal, delete the Shift on 20th December 2022.
-                     Delete the created Shift. Delete the created Employer.
-
-        Assertion: Assert the date and time of the Calendar event as 9:00 - 17:00, 20th December.
-        */
+        // 1. Create Employer "Sheffield" and Shift "Weekday" (steps 1 - 10 in test case createShifts).
+        // 2. Press back to return to the homepage.
+        // 3. Click the Edit icon in the bottom-right corner of the homepage.
+        // 4. Click the S icon.
+        // 5. Click "Weekday".
+        // 6. Click date 30 of January 2023.
+        // 7. Click the ✓ icon.
+        // 8. Click the Edit icon in the bottom-right corner of the homepage.
+        // 9. Click the WD icon.
+        // 10. Click "Delete".
+        // 11. Click date 30 of January 2023.
+        // 12. Click the ✓ icon.
+        // 13. Go to the Shifts page (steps 3 - 4 in test case createShifts).
+        // 14. Delete Shift "Weekday" and Employer "Sheffield" then return to the homepage (steps 11 - 15 in test case createShifts).
     }
 }
