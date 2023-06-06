76e9ead5a65c3c7e30acd5121e5fcf041b252e47
guruizhen <ravenna1031@gmail.com>
2023-06-06 15:30:02 +0100

    fix UI Automator typo

--- app/src/androidTest/java/de/nulide/shiftcal/TestSuite.java
+++ app/src/androidTest/java/de/nulide/shiftcal/TestSuite.java
@@ -1,23 +1,9 @@
 package de.nulide.shiftcal;
 
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.longClick;
-import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
-import android.view.KeyEvent;
-
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
-import androidx.test.uiautomator.Until;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -52,31 +38,31 @@ public class TestSuite {
         // 7. Click the menu icon in the top-right corner - Espresso
         // 8. Click "Shifts" - Espresso
         // 9. Click the + icon on the bottom-right corner - Espresso
-        // 10. Enter "Weekday" in the Name field - UIAutomator
-        // 11. Enter "WD" in the Short Name field - UIAutomator
-        // 12. Click the button of Start Time, click 9 then click OK - UIAutomator
-        // 13. Click the button of End Time, click 17 then click OK - UIAutomator
-        // 14. Click the ✓ icon - UIAutomator
-        // 15. Press back to return to the homepage - UIAutomator
+        // 10. Enter "Weekday" in the Name field - UI Automator
+        // 11. Enter "WD" in the Short Name field - UI Automator
+        // 12. Click the button of Start Time, click 9 then click OK - UI Automator
+        // 13. Click the button of End Time, click 17 then click OK - UI Automator
+        // 14. Click the ✓ icon - UI Automator
+        // 15. Press back to return to the homepage - UI Automator
         // 16. Click the Edit icon in the bottom-right corner of the homepage - Espresso
         // 17. Click the S icon - Espresso
         // 18. Click "Weekday" - Espresso
-        // 19. Click date 30 of January 2023 - UIAutomator
+        // 19. Click date 30 of January 2023 - UI Automator
         // 20. Click the ✓ icon - Espresso
-        // 21. Click the Edit icon in the bottom-right corner of the homepage - UIAutomator
-        // 22. Click the WD icon - UIAutomator
-        // 23. Click "Delete" - UIAutomator
-        // 24. Click date 30 of January 2023 - UIAutomator
-        // 25. Click the ✓ icon - UIAutomator
-        // 26. Click the menu icon in the top-right corner - UIAutomator
-        // 27. Click "Shifts" - UIAutomator
+        // 21. Click the Edit icon in the bottom-right corner of the homepage - UI Automator
+        // 22. Click the WD icon - UI Automator
+        // 23. Click "Delete" - UI Automator
+        // 24. Click date 30 of January 2023 - UI Automator
+        // 25. Click the ✓ icon - UI Automator
+        // 26. Click the menu icon in the top-right corner - UI Automator
+        // 27. Click "Shifts" - UI Automator
         // 28. Long click "Weekday" - Espresso
         // 29. Click "Delete" - Espresso
         // 30. Press back to return to the homepage - Espresso
-        // 31. Click the menu icon in the top-right corner - UIAutomator
-        // 32. Click "Employers" - UIAutomator
+        // 31. Click the menu icon in the top-right corner - UI Automator
+        // 32. Click "Employers" - UI Automator
         // 33. Long click "Sheffield" - Espresso
-        // 34. Click "Delete" - UIAutomator
-        // 35. Press back to return to the homepage - UIAutomator
+        // 34. Click "Delete" - UI Automator
+        // 35. Press back to return to the homepage - UI Automator
     }
 }
